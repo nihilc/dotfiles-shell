1900997f1bbb5f65e5e00c1e882213c62a399920
Christian Cardenas <nihilist.cc@gmail.com>
2023-09-16 00:10:58 -0500

    zsh autostart nvm

--- zsh/config/autocmd.zsh
+++ zsh/config/autocmd.zsh
@@ -6,3 +6,7 @@ then
     pkill ssh-agent
     eval "$(ssh-agent -s)" > /dev/null 2>&1
 fi
+
+# Node version manager
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
