e997ee3c6a0ae37dac415e69aecb84581aa1e48a
Christian Cardenas <nihilist.cc@gmail.com>
2024-03-12 20:58:51 -0500

    nvim: add git keymaps description

--- nvim/lua/nihilc/plugins/git.lua
+++ nvim/lua/nihilc/plugins/git.lua
@@ -15,10 +15,9 @@ return {
           if vim.bo.ft ~= "fugitive" then return end
 
           local bufnr = vim.api.nvim_get_current_buf()
-          local opts = {buffer = bufnr, remap = false}
 
-          vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git("push") end, opts)
-          vim.keymap.set("n", "<leader>gP", function() vim.cmd.Git("pull") end, opts)
+          vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git("push") end, { buffer = bufnr, remap = false, desc = "Git push"})
+          vim.keymap.set("n", "<leader>gP", function() vim.cmd.Git("pull") end, { buffer = bufnr, remap = false, desc = "Git pull"})
         end,
       })
     end,
