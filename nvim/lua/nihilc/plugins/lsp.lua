ed40f968798f1a6fae9f079ec65c3602f4577712
Christian Cardenas <nihilist.cc@gmail.com>
2024-07-26 00:04:45 -0500

    nvim: add jdtls

--- nvim/lua/nihilc/plugins/lsp.lua
+++ nvim/lua/nihilc/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
+    'mfussenegger/nvim-jdtls'
   },
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -119,5 +120,12 @@ return {
         },
       },
     })
+
+    -- Configure jdtls
+    local config = {
+      cmd = { '/home/nihilc/.local/share/nvim/mason/bin/jdtls' },
+      root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
+    }
+    require('jdtls').start_or_attach(config)
   end,
 }
