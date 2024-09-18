a1714ceb3868e39258f146191ec7556f3954b9a4
Christian Cardenas <nihilist.cc@gmail.com>
2024-09-17 21:44:35 -0500

    zsh: autostart ssh-agent if isn't started

--- zsh/config/autocmd.zsh
+++ zsh/config/autocmd.zsh
@@ -2,5 +2,8 @@
 
 # Try to connect to tmux session or start a new one
 if command -v tmux &> /dev/null && [ -z "$TMUX" ]; then
+  if [ -z "$(pgrep ssh-agent)" ]; then
+    eval "$(ssh-agent)"
+  fi
   tmux a || tmux
 fi
