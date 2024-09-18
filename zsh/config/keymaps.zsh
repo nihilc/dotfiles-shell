cabf002d70fa83ff55ca709fabe796c548073aba
Christian Cardenas <nihilist.cc@gmail.com>
2024-09-17 21:36:28 -0500

    zsh: add keybindings

--- zsh/config/keymaps.zsh
+++ zsh/config/keymaps.zsh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
+bindkey "^P" history-beginning-search-backward
+bindkey "^N" history-beginning-search-forward
+
 # Enable vim mode
 bindkey -v
-
+bindkey -M viins '^?' backward-delete-char
 # Use vim keys in tab complete menu
 bindkey -M menuselect 'h' vi-backward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
-
-# Remove keymaps
-bindkey -r "^L"
