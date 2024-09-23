7b5c7552e1e4dbc9024433c63c9d085f305dc1b0
Christian Cardenas <nihilist.cc@gmail.com>
2024-09-22 22:22:23 -0500

    nvim: disable jdtls. no more java for now :3

--- nvim/lua/nihilc/plugins/lsp.lua
+++ nvim/lua/nihilc/plugins/lsp.lua
@@ -171,6 +171,6 @@ return {
       cmd = { '/home/nihilc/.local/share/nvim/mason/bin/jdtls' },
       root_dir = vim.fs.dirname(vim.fs.find({ 'gradlew', '.git', 'mvnw' }, { upward = true })[1]),
     }
-    require('jdtls').start_or_attach(config)
+    -- require('jdtls').start_or_attach(config)
   end,
 }
